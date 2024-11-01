ba980dc8a46081d8f202455772d4db30c4a6f3ff
Rama Majid <127276782+RamaMajid@users.noreply.github.com>
2024-11-01 18:30:21 +0700

    Update CourseController.php

--- Tugas/Tugas 1/Controller/CourseController.php
+++ Tugas/Tugas 1/Controller/CourseController.php
@@ -5,32 +5,42 @@ namespace Controller;
 use Models\OnlineCourse;
 use Models\User;
 
-class CourseController{
+class CourseController {
     private $listCourse = [];
     private $listUser = [];
 
-    public function addCourse($courseName, $duration, $platform){
+    public function addCourse($courseName, $duration, $platform) {
         $course = new OnlineCourse($courseName, $duration, $platform);
         $this->listCourse[] = $course;
         return $course;
     }
 
-    public function addUser($userName, $email){
+    public function addUser($userName, $email) {
         $user = new User($userName, $email);
         $this->listUser[] = $user;
         return $user;
     }
 
-    public function showCourse(){
-        foreach($this->listCourse as $course){
-            echo $course . "\n";
-        }
+    public function getCoursesAsJson() {
+        $courses = array_map(function($course) {
+            return [
+                'courseName' => $course->getCourseName(),
+                'duration' => $course->getDuration(),
+                'platform' => $course->getPlatform()
+            ];
+        }, $this->listCourse);
+
+        return json_encode(['courses' => $courses], JSON_PRETTY_PRINT);
     }
 
-    public function showUser(){
-        foreach($this->listUser as $user){
-            echo $user->userInfo() . "\n";
-        }
-    }
+    public function getUsersAsJson() {
+        $users = array_map(function($user) {
+            return [
+                'userName' => $user->getUserName(),
+                'email' => $user->getEmail()
+            ];
+        }, $this->listUser);
 
+        return json_encode(['users' => $users], JSON_PRETTY_PRINT);
+    }
 }
