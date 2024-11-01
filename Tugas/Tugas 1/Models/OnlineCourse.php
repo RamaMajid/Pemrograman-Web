f1922568dddeeb3fd3515753f73945b25de2f002
Rama Majid <127276782+RamaMajid@users.noreply.github.com>
2024-11-01 18:31:14 +0700

    Update OnlineCourse.php

--- Tugas/Tugas 1/Models/OnlineCourse.php
+++ Tugas/Tugas 1/Models/OnlineCourse.php
@@ -4,21 +4,25 @@ namespace Models;
 
 use \Traits\LoggingTrait;
 
-class OnlineCourse extends Course{
+class OnlineCourse extends Course {
     use LoggingTrait;
 
     private $platform;
 
-    public function __construct($courseName, $duration, $platform){
+    public function __construct($courseName, $duration, $platform) {
         parent::__construct($courseName, $duration);
         $this->platform = $platform;
     }
 
-    public function description(){
+    public function description() {
         return "Kursus Online: {$this->courseName} di platform {$this->platform} selama {$this->duration} jam";
     }
 
-    public function __tostring(){
+    public function __toString() {
         return $this->description();
+    }
+
+    public function getPlatform() {
+        return $this->platform;
     }
 }
