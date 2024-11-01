de7607b9c229f3d335a811c26beb65b8b496456a
Rama Majid <127276782+RamaMajid@users.noreply.github.com>
2024-11-01 18:31:31 +0700

    Update User.php

--- Tugas/Tugas 1/Models/User.php
+++ Tugas/Tugas 1/Models/User.php
@@ -2,16 +2,24 @@
 
 namespace Models;
 
-class User{
+class User {
     private $userName;
     private $email;
 
-    public function __construct($userName, $email){
-        $this -> userName = $userName;
-        $this -> email = $email;
+    public function __construct($userName, $email) {
+        $this->userName = $userName;
+        $this->email = $email;
     }
 
-    public function userInfo(){
+    public function userInfo() {
         return "Nama: $this->userName, Email: $this->email";
+    }
+
+    public function getUserName() {
+        return $this->userName;
+    }
+
+    public function getEmail() {
+        return $this->email;
     }
 }
