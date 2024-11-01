350514475bdf5caa9924b3b188c049f391252bde
Rama Majid <127276782+RamaMajid@users.noreply.github.com>
2024-11-01 18:30:55 +0700

    Update Course.php

--- Tugas/Tugas 1/Models/Course.php
+++ Tugas/Tugas 1/Models/Course.php
@@ -2,14 +2,22 @@
 
 namespace Models;
 
-abstract class Course{
+abstract class Course {
     protected $courseName;
     protected $duration;
 
-    public function __construct($courseName, $duration){
-        $this->courseName;
-        $this->duration;
+    public function __construct($courseName, $duration) {
+        $this->courseName = $courseName;
+        $this->duration = $duration;
     }
 
     abstract public function description();
+
+    public function getCourseName() {
+        return $this->courseName;
+    }
+
+    public function getDuration() {
+        return $this->duration;
+    }
 }
